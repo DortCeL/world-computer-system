ed28533f629dafbe79562694cc8c95334093b170
DortCeL <exh.alif.47@gmail.com>
2024-09-06 18:45:50 +0600

    min-h-screen lol

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Nav from "../components/Nav";
 
 export default function Home() {
 	return (
-		<section className='bg-custom-gradient'>
+		<section className='bg-custom-gradient min-h-screen'>
 			<Nav />
 			<CustomPCBuild />
 		</section>
--- client/src/pages/IntelPcBuild.jsx
+++ client/src/pages/IntelPcBuild.jsx
@@ -3,7 +3,7 @@ import Nav from "../components/Nav";
 export default function IntelPcBuild() {
 	return (
 		<>
-			<section className='bg-custom-gradient h-screen'>
+			<section className='bg-custom-gradient min-h-screen'>
 				<Nav />
 			</section>
 		</>
